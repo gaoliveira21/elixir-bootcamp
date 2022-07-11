daef1e2cb0bf6ffa8e66d71adb420503256eaee4
Gabriel Jose de Oliveira <Gabriel Jose de Oliveira>
2022-07-11 20:01:51 -0300

    refactor: implement second solution

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,14 +3,9 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearths", "Diamonds"]
 
-    cards =
-      for value <- values do
-        for suit <- suits do
-          "#{value} of #{suit}"
-        end
-      end
-
-    List.flatten(cards)
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
