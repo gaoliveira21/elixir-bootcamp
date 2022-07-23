540ded1bcf5a68322a07144f97d2fcdcfaafb1a3
Gabriel Jose de Oliveira <Gabriel Jose de Oliveira>
2022-07-23 14:07:52 -0300

    feat: add grid property at Image struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,13 +3,37 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(image) do
-    %Identicon.Image{ hex: hex_list } = image
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3)
+      |> split_grid
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def split_grid(grid) do
+    {new_grid, _rest} = Enum.split(grid, 5)
+
+    new_grid
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: hex_list} = image) do
     [r, g, b | _tail] = hex_list
 
-    [r, g, b]
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
@@ -17,6 +41,6 @@ defmodule Identicon do
       :crypto.hash(:md5, input)
       |> :binary.bin_to_list()
 
-    %Identicon.Image{ hex: hex }
+    %Identicon.Image{hex: hex}
   end
 end
