2b5ec7b82d9052861b41888dec2b61e878bf175c
Gabriel Jose de Oliveira <Gabriel Jose de Oliveira>
2022-07-24 08:16:56 -0300

    feat: draw and save image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,27 +6,48 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{pixel_map: pixel_map, color: color} = _image) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(
+      pixel_map,
+      fn({start, stop}) ->
+        :egd.filledRectangle(image, start, stop, fill)
+      end
+    )
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    pixel_map = Enum.map(
-      grid,
-      fn {_code, index} ->
-        horizontal = rem(index, 5) * 50
-        vertical = div(index, 5) * 50
+    pixel_map =
+      Enum.map(
+        grid,
+        fn {_code, index} ->
+          horizontal = rem(index, 5) * 50
+          vertical = div(index, 5) * 50
 
-        top_left = {horizontal, vertical}
-        bottom_right = {horizontal + 50, vertical + 50}
+          top_left = {horizontal, vertical}
+          bottom_right = {horizontal + 50, vertical + 50}
 
-        {top_left, bottom_right}
-      end
-    )
+          {top_left, bottom_right}
+        end
+      )
 
     %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    Enum.filter(
+    grid = Enum.filter(
       grid,
       fn {code, _index} -> rem(code, 2) == 0 end
     )
